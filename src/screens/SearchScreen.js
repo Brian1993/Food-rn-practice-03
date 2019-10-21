772bca713215e8de76aaeec7bbfebfe25b89e54e
Brian Chen <brian.chen@104.com.tw>
2019-10-21 10:11:54 +0800

    extract businesses logic to custom hooks: useResults

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,18 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
-  const [results, setResults] = useState([])
-  const [errMsg, setErrMsg] = useState('')
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get('/search', { 
-        params: {
-          limit: 50,
-          term,
-          location: 'san jose'
-        }
-      })
-      setResults(response.data.businesses)
-    } catch (e) {
-      setErrMsg('Something went wrong')
-    }
-  }
+  const [searchApi, results, errMsg] = useResults()
 
   return (
     <View>
       <SearchBar 
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={searchApi}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
       />
       {errMsg ? <Text>{errMsg}</Text> : null}
       <Text>We have found: {results.length} results</Text>
